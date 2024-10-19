45e14cb7acb8de2eed9f4d35d819e5e0658c1c24
Oleksandr Cheban <cebanoleksandr@email.com>
2024-10-19 14:39:07 +0300

    #file_system

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,8 +1,36 @@
 import os from 'os';
 import path from 'path';
+import fs from 'fs';
 
 const filePath = path.join(os.homedir(), 'weather-data.json');
 
-export const saveKeyValue = (key, value) => {
-  console.log(filePath);  
+export const saveKeyValue = async (key, value) => {
+  let data = {};
+
+  if (await isExist(filePath)) {
+    const file = await fs.promises.readFile(filePath);
+    data = JSON.parse(file);
+  }
+
+  data[key] = value;
+
+  await fs.promises.writeFile(filePath, JSON.stringify(data));
+}
+
+export const getKeyValue = async (key) => {
+  if (await isExist(filePath)) {
+    const file = await fs.promises.readFile(filePath);
+    const data = JSON.parse(file);
+    return data[key];
+  }
+  return undefined;
+}
+
+const isExist = async (path) => {
+  try {
+    await fs.promises.stat(filePath);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
